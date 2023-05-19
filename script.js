125ae642206b7804bb45043cddfbcdfe967d78e5
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-05-19 15:35:19 +0200

    master: Adding STEP #2 send a word to an API with fecth()

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
 /* Étape #1 : Récupération d'un mot */
+const watchSubmit = () => {
+    const form = document.querySelector("#form")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        const data = new FormData(form)
+        const wordToSearch = data.get("search")
+        apiCall(wordToSearch)
+    })
+}
 
-const form = document.querySelector("#form")  
-form.addEventListener("submit", (event) => {
-    event.preventDefault()
-    const data = new FormData(form)
-    const wordToSearch = data.get("search")
-})
+/* Étape #2 : Envoyer un mot à l'API */
+const apiCall = (word) => {
+    console.log("WORD TO SEARCH", word)
+    fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data)
+    })
+}
+
+// Lancement du programme
+watchSubmit()
