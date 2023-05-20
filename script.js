0f6cd8b3718c5b930f2223f49befdf9227512081
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-05-20 08:07:40 +0200

    master: added .catch() method in end of STEP #3

--- script.js
+++ script.js
@@ -11,13 +11,19 @@ const watchSubmit = () => {
 
 /* Étape #2 : Envoyer un mot à l'API */
 const apiCall = (word) => {
-    console.log("WORD TO SEARCH", word)
     fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${word}`)
-    .then((response) => response.json())
-    .then((data) => {
-        console.log(data)
-    })
+        .then((response) => response.json()) 
+        .then((data) => {
+            /*  Étape #3 : Récupérer la donnée */ 
+            const informationsNeeded = extractData(data[0])
+            renderToHTML(informationsNeeded)
+        })
+        .catch((error) => {
+            alert('Le mot demandé n\'existe pas')
+            console.error(error)
+        })
 }
+
 
 // Lancement du programme
 watchSubmit()
