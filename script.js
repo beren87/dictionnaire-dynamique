7ca443022e7f1afd236b1a8ff0e205f22e971190
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-05-29 11:19:11 +0200

    master: added findProp with boucles and condition

--- script.js
+++ script.js
@@ -24,6 +24,35 @@ const apiCall = (word) => {
         })
 }
 
+const extractData = (data) => {
+    // 1 - Mot 
+    const word = data.word
+    // 2 - Écriture phonétique
+    const phonetic = findProp(data.phonetics, "text")
+    // 3 - Prononciation (audio)
+    const pronoun = findProp(data.phonetics, "audio")
+    // 4 - Définition(s)
+    const meanings = data.meanings
+
+    return {
+        word: word,
+        phonetic: phonetic,
+        pronoun: pronoun, 
+        meanings: meanings
+    }
+}
+const findProp = (array, name) => {
+    // Elle parcours un tableau d'objets
+    for (let i = 0; i < array.length; i++) {
+        // Et cherche dans ce tableau, si l'objet en cours contient une certaines propriété
+        const currentObject = array[i]
+        const hasProp = currentObject.hasOwnProperty(name)
+        // Alors elle renvoit cette propriété
+        if (hasProp) return currentObject[name]
+    }
+}
+
+
 
 // Lancement du programme
 watchSubmit()
