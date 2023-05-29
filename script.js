223ae58a39fe07c264a59edc0378953d626aa270
Berenger FERGUENIS <berenger@MacBook-Pro-de-Berenger.local>
2023-05-29 11:25:13 +0200

    master: step 4: display information, create dynamic HTML element and add JS audio

--- script.js
+++ script.js
@@ -52,7 +52,54 @@ const findProp = (array, name) => {
     }
 }
 
+/* ÉTAPE 4 : Afficher les informations de mon mot sur ma page */
+const renderToHTML = (data) => {
+    const card = document.querySelector('.js-card')
+    card.classList.remove('card--hidden')
+
+    // Manipulation de textes avec la propriété textContent
+    const title = document.querySelector(".js-card-title")
+    title.textContent = data.word
+    const phonetic = document.querySelector(".js-card-phonetic")
+    phonetic.textContent = data.phonetic
+    
+    // Création d'élements HTML dynamiques
+    const list = document.querySelector('.js-card-list')
+    list.innerHTML = ""
+    for(let i = 0; i < data.meanings.length; i++) {
+        const meaning = data.meanings[i]
+        const partOfSpeech = meaning.partOfSpeech
+        const definition = meaning.definitions[0].definition
+
+    // La création d'élements 
+        const li = document.createElement('li')
+        li.classList.add('card__meaning')
+        const pPartOfSpeech = document.createElement('p')
+        pPartOfSpeech.textContent = partOfSpeech
+        pPartOfSpeech.classList.add('card__part-of-speech')
+        const pDefinition = document.createElement('p')
+        pDefinition.textContent = definition
+        pDefinition.classList.add('card__definition')
+        
+        li.appendChild(pPartOfSpeech)
+        li.appendChild(pDefinition)
+        list.appendChild(li)
+    }
+
+    // Ajout de l'audio en JS
+    const button = document.querySelector('.js-card-button')
+    const audio = new Audio(data.pronoun)
+    button.addEventListener('click', () => {
+        button.classList.remove("card__player--off")
+        button.classList.add("card__player--on")
+        audio.play()
+    })
+    audio.addEventListener('ended', () => {
+        button.classList.remove("card__player--on")
+        button.classList.add("card__player--off")
+    })
+}
 
 
-// Lancement du programme
+// LANCEMENT DU PROGRAMME
 watchSubmit()
